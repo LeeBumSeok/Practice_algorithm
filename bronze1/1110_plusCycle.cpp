57819300413032d94cde7cbdcdc71fda9b562172
LeeBumSeok <qpwoeiru6486@gmail.com>
2021-09-03 20:43:45 +0900

    Solve

--- bronze1/1110_plusCycle.cpp
+++ bronze1/1110_plusCycle.cpp
@@ -2,19 +2,22 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int input, before, after, tmp, cycle = 0;
     cin >> input;
 
     tmp = input;
 
-    while(true) {
+    while (true)
+    {
         before = tmp / 10;
         after = tmp % 10;
         tmp = after * 10 + (before + after) % 10;
         cycle++;
 
-        if(input == tmp) break;
+        if (input == tmp)
+            break;
     }
 
     cout << cycle << endl;
